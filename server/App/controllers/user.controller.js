8d5d48024c5f407f08730c15cf4adee2e1cc2f0e
arav20603 <dakshandaravind28@gmail.com>
2025-08-05 19:47:01 +0530

    Implemented APIs for login, logout

--- server/App/controllers/user.controller.js
+++ server/App/controllers/user.controller.js
@@ -33,3 +33,36 @@ export const signUp = async (req, res) => {
   }
 
 }
+
+export const login = async (req, res) => {
+  const {email, password} = req.body
+  try {
+    const user = await userModel.findOne({email})
+    if (!user) {
+      return res.status(400).json({success: false, msg: "User does not exist"})
+    }
+    const isPassword = await bcrypt.compare(password, user.password)
+    if (!isPassword) {
+      return res.status(400).json({success: false, msg: "Invalid password"})
+    }
+
+    res.status(201).json({
+      success: true,
+      msg: "User logged in succesfully",
+      user: {
+        ...user._doc,
+        password: undefined
+      }
+    })
+  } catch (error) {
+    res.status(400).json({success: false, msg: "Error in user login", error: error})
+  }
+}
+
+export const logout = async (req, res) => {
+  try {
+    res.status(201).json({success: true, msg: "Successfully logged out"})
+  } catch (error) {
+     res.status(400).json({success: false, msg: "Error in user logout", error: error})
+  }
+}
--- server/App/routes/user.routes.js
+++ server/App/routes/user.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { signUp } from '../controllers/user.controller.js'
+import { login, logout, signUp } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
-router.post('/createUser', signUp)
+router.post('/signup', signUp)
+router.post('/login', login)
+router.post('/logout', logout)
 
 export default router
